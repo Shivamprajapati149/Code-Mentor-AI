efe894955068cb5e4bf9c83d2f6dd5b28c639f66
Shivam Prajapati <bhindshivam426@gmail.com>
2025-12-09 22:48:11 +0530

    update frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import Sidebar from "/components/Sidebar";
-import ChatBox from "/components/ChatBox";
+import Sidebar from "./components/Sidebar";
+import ChatBox from "./components/ChatBox";
 import "./app.css";
 
 function App() {
